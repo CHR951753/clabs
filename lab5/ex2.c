192ba19f0ab828f3176561e963111fd6626fb6da
CHR951753 <56293353+CHR951753@users.noreply.github.com>
2019-12-26 03:16:23 -0800

    Update ex2.c

--- lab5/ex2.c
+++ lab5/ex2.c
@@ -1,45 +1,89 @@
 #include <stdio.h>
 #include <ctype.h>
+#define NUMBER '0'
 #define BUFSIZE 100
 char buf[BUFSIZE];
 int bufp = 0;
-int getch(void)
+int getch(void);
+void ungetch(int);
+char getfloat(double *pn);
+int main(void)
 {
-    return (bufp>0)?buf[--bufp] : getchar();
+    int n;
+    double s[100];
+    double w = 0;
+
+    for (n = 0; n < 100 ; n++)
+    {
+       
+        if(getfloat(&s[n]) == '\n'){
+            w = w + s[n];
+            break;
+        }
+         w = w + s[n];
+    }
+
+    printf("%f", w);
+    return 0;
 }
-void ungetch(int c)
+
+char getfloat(double *pn)
 {
-    if (bufp >=BUFSIZE)
-     printf("ungetch:too many characters\n");
-     else
-        buf[bufp++] = c;
-}
-int getfloat(float *pf)
-{
-    int c, sign;
-    float  power;
-    while (isspace(c = getch()));
-    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+    char c;
+    int a;
+    int b = 1;
+    while (isspace(c = getch()))
+    {
+        ;
+    }
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.')
+    {
         ungetch(c);
         return 0;
     }
-    sign = (c == '-') ? -1 : 1;
-    if (c == '+' || c == '-') 
+    a = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+    {
         c = getch();
-    for (*pf = 0; isdigit(c); c = getch())
-        *pf = *pf * 10 + (c - '0');
-    *pf *= sign;
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+    {
+        *pn = 10 * *pn + (c - '0');
+    }
+    if (c == '.')
+    {
+        c = getchar();
+        if (!isdigit(c))
+        {
+            ungetch(c);
+        }
+        for (; isdigit(c); c = getchar())
+        {
+            b *= 10;
+            *pn = 10 * *pn + (c - '0');
+        }
+    }
+    *pn = *pn / b * a;
     if (c != EOF)
+    {
         ungetch(c);
+    }
     return c;
 }
-#define SIZE  10
-int main()
-{
-    int i;
-    float array[SIZE];
 
-    for (i = 0; i < SIZE && getfloat(&array[i]) != EOF; ++i)
-            printf("%.2f\n", array[i]);
-    return 0;
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+    {
+        printf("ungetch too many characters\n");
+    }
+    else
+    {
+        buf[bufp++] = c;
+    }
 }
